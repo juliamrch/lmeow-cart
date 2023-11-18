588944074005ab4ab08b655f84c9c07c5afc7bd7
julia <julia.navarro@clever-cloud.com>
2023-11-18 18:32:59 +0300

    shop page: unfuck cards????

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -2,19 +2,19 @@
 
 import React, { useEffect, useState } from "react";
 import { title, subtitle } from "@/components/primitives";
-import { Card, CardBody, CardFooter, Image } from "@nextui-org/react";
+import { Card, CardHeader, CardBody, CardFooter, Image, Button } from "@nextui-org/react";
 import { Spacer } from "@nextui-org/react";
 
 interface Product {
-  id: number;
   name: string;
   price: number;
+  image: string;
   // add other properties if needed
 }
 
-export default function PublicShopPage() {
-  const [data, setData] = useState(null);
-
+export default function App() {
+  //const [data, setData] = useState(null);
+  const [data, setData] = useState<Product[] | null>(null);
   useEffect(() => {
     fetch("http://localhost:3000/api/product")
       .then((response) => response.json())
@@ -38,23 +38,28 @@ export default function PublicShopPage() {
 
       <Spacer y={40} />
 
-      <div className="gap-2 grid grid-cols-2 sm:grid-cols-4">
+      <div className="gap-2 grid grid-cols-2 sm:grid-cols-1">
         {data.map((product: Product, index: number) => (
           <div key={index}>
-            <Card shadow="sm" isPressable onPress={() => console.log("item pressed")}>
-              <CardBody className="overflow-visible p-0">
-                <Image
-                  shadow="sm"
-                  radius="lg"
-                  width="100%"
-                  alt={product.name}
-                  className="w-full object-cover h-[140px]"
-                  src={product.image}
-                />
-              </CardBody>
-              <CardFooter className="text-small justify-between">
-                <b>{product.name}</b>
-                <p className="text-default-500">{product.price}</p>
+            <Card isFooterBlurred className="w-full h-[400px] col-span-12 sm:col-span-5">
+              <CardHeader className="absolute z-10 top-1 flex-col items-start">
+                <p className="text-tiny text-white/60 uppercase font-bold"></p>
+                <h4 className="text-black font-medium text-2xl">{product.name}</h4>
+              </CardHeader>
+              <Image
+                removeWrapper
+                alt="Card example background"
+                className="z-0 w-full h-full scale-125 -translate-y-6 object-cover"
+                alt={product.name}
+                src={product.image ? "api/product/image/" + product.id : "https://placehold.co/600x400/png"}
+              />
+              <CardFooter className="absolute bg-white/30 bottom-0 border-t-1 border-zinc-100/50 z-10 justify-between">
+                <div>
+                  <p className="text-black font-bold">{product.price} ETH</p>
+                </div>
+                <Button className="text-tiny" color="primary" radius="full" size="sm">
+                  Buy
+                </Button>
               </CardFooter>
             </Card>
           </div>
