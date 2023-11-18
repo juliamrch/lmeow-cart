dd983f7eab0a3aba3b28e0f698f6977c93f1df5b
julia <julia.navarro@clever-cloud.com>
2023-11-18 09:47:05 +0300

    create public product fetch

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,9 +1,47 @@
-import { title } from "@/components/primitives";
+"use client";
 
-export default function PricingPage() {
+import React, { useEffect, useState } from "react";
+import { title, subtitle } from "@/components/primitives";
+
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+  // add other properties if needed
+}
+
+export default function ShopPage() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/product")
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => console.error("Error:", error));
+  }, []);
+
+  // If data is still null, render a loading message
+  if (data === null) {
+    return (
+      <div>
+        <h1 className={title()}>There are no products in this store yet.</h1>
+        <h2 className={subtitle({ class: "mt-4" })}>
+          Connect wallet and start adding some if you are the admin. Come back later if you are not.
+        </h2>
+      </div>
+    );
+  }
+
+  // Otherwise, render the data
   return (
     <div>
       <h1 className={title()}>Shop</h1>
+      {data.map((product) => (
+        <div key={product.id}>
+          <h2>{product.name}</h2>
+          <p>{product.price}</p>
+        </div>
+      ))}
     </div>
   );
 }
